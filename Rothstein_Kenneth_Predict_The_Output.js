b7a1da61ded3a224b19737ce78cc89a4279aa7a8
Kenneth Rothstein <skottrod@me.com>
2021-03-12 11:52:13 -0800

    Code snippet 3

--- Rothstein_Kenneth_Predict_The_Output.js
+++ Rothstein_Kenneth_Predict_The_Output.js
@@ -9,3 +9,13 @@ for (var i = 1; i < 10; i += 2) {
   }
 }
 //my predicted output is 3 and 9.
+
+for (var j = 1; j <= 15; j++) {
+  if (j % 2 == 0) {
+    j += 2;
+  } else if (j % 3 == 0) {
+    j++;
+  }
+  console.log(j);
+}
+//my predicted output is 1, 4, 5, 8, 10, 11, 14, 16
