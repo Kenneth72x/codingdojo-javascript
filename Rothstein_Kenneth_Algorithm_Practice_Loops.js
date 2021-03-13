32d95af44d0046a76bc1e4109d379563f6a77675
Kenneth Rothstein <skottrod@me.com>
2021-03-12 21:10:04 -0800

    Algo 2

--- Rothstein_Kenneth_Algorithm_Practice_Loops.js
+++ Rothstein_Kenneth_Algorithm_Practice_Loops.js
@@ -3,4 +3,9 @@ for (var i = 1; i < 20; i += 2) {
   console.log(i);
 }
 
-//Sum numbers form 1 to 5, print out the sum and current number
+//Sum numbers from 1 to 5, print out the sum and current number
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+  sum += i;
+  console.log("Current Num:", i, "Sum:", sum);
+}
