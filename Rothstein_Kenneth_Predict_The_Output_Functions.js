c6f96b21db232d2a7e70e61551116116d28ebba4
Kenneth Rothstein <skottrod@me.com>
2021-03-14 13:03:41 -0700

    Predict 2

--- Rothstein_Kenneth_Predict_The_Output_Functions.js
+++ Rothstein_Kenneth_Predict_The_Output_Functions.js
@@ -6,3 +6,18 @@ function greeting() {
 var word = greeting();
 console.log(word);
 //my predicted output: Hello World
+
+//Predict 2
+function add(num1, num2) {
+  console.log("Summing Numbers!");
+  console.log("num1 is: " + num1);
+  console.log("num2 is: " + num2);
+  var sum = num1 + num2;
+  return sum;
+}
+var result1 = add(3, 5);
+var result2 = add(4, 7);
+console.log(result1);
+console.log(result2);
+//my predicted output:
+//Summing Numbers!, num1 is: 3, num2 is: 5, 8, Summing Numbers!, num1 is: 4, num2 is: 7, 11, 8, 11
