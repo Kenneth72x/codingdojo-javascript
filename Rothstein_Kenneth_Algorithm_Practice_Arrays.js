9c382011a35f286b065ec64aa4cae1e6f27711c6
Kenneth Rothstein <skottrod@me.com>
2021-03-14 11:03:18 -0700

    Challenge 2

--- Rothstein_Kenneth_Algorithm_Practice_Arrays.js
+++ Rothstein_Kenneth_Algorithm_Practice_Arrays.js
@@ -1,6 +1,13 @@
+//challenge 1
 var testArr = [6, 3, 5, 1, 2, 4];
 var sum = 0;
 for (let i = 0; i < testArr.length; i++) {
   sum += testArr[i];
   console.log("val:", testArr[i], "sum:", sum);
 }
+
+//challenge 2
+for (let i = 0; i < testArr.length; i++) {
+  testArr[i] = testArr[i] * i;
+}
+console.log(testArr);
