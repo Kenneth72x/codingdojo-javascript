be6e9198ca83e970e637176f3cba24732574f705
Kenneth Rothstein <skottrod@me.com>
2021-03-13 16:18:58 -0800

    Snippet 3

--- Rothstein_Kenneth_Predict_The_Output_Arrays.js
+++ Rothstein_Kenneth_Predict_The_Output_Arrays.js
@@ -1,9 +1,11 @@
+//Snippet 1
 var arr = [8, 6, 7, 5, 3, 0, 9];
 for (var i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
 //predicted output: (8, 6, 7, 5, 3, 0, 9)
 
+//Snippet 2
 var arr = [7, 3, 8, 4, 2, 0, 1];
 for (var i = 0; i < arr.length; i++) {
   if (arr[i] % 2 == 0) {
@@ -14,6 +16,7 @@ for (var i = 0; i < arr.length; i++) {
 }
 //predicted output: (That is odd!, That is odd!, 8, 4, 2, 0, That is odd!)
 
+//Snippet 3
 var arr = [1, 3, 8, -5, 0, -2, 4, -1];
 var newArr = [];
 for (var i = 0; i < arr.length; i++) {
