48ce924c71048cdcd904c0df3e83f1d8d1e6ff4a
Kenneth Rothstein <skottrod@me.com>
2021-03-14 17:47:59 -0700

    Objective 9

--- Rothstein_Kenneth_Basic_Foundation_1.js
+++ Rothstein_Kenneth_Basic_Foundation_1.js
@@ -91,6 +91,13 @@ console.log(greaterThanY([1, 3, 5, 7], 2));
 
 //Objective 9: Squares: Given an array with multiple values, write a function that replaces each value in the array with the value squred by itself.
 //  (ex. [1,5,10,-2] will become [1,25,100,4]).
+function squares(arr) {
+  for (var idx = 0; idx < arr.length; idx++) {
+    arr[idx] = arr[idx] * arr[idx];
+  }
+  console.log(arr);
+}
+squares([1, -2, 5, 10]);
 
 //Objective 10: Negatives: Given an array with multiple values, write a function that replaces any negative numbers within the array with the value of 0.
 //  When the program is done the array should contain no negative values. (e.g. [1,5,10,-2] will become [1,5,10,0]).
